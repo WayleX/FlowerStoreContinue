a0543ed0c11952ac4d2dee1e15d86cc335d1d59c
waylex <shykula.pn@ucu.edu.ua>
2023-10-26 22:41:27 +0300

    gg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import com.example.demo.flowers.Flower;
-//import com.example.demo.flowers.FlowerColor;
-//import com.example.demo.flowers.FlowerType;
-
 @SpringBootApplication
 @RestController
 public class DemoApplication {
